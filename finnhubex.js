47934c70b70064f1496f21dca9051c544eecb583
0marA <theengineermachine@gmail.com>
2022-03-14 14:55:39 -0500

    Pass in symbol and returns open price

--- finnhubex.js
+++ finnhubex.js
@@ -1,17 +1,25 @@
 const finnhub = require('finnhub');
-const { API_KEY } = require('./keys.js')
+const { API_KEY } = require('./keys.js');
+const { promisify } = require('util')
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
+const sleep = promisify(setTimeout)
 
-//Quote
-finnhubClient.quote('AAPL', 
-function c(data){
-	console.log(data)
-	
+var x;
+
+getOpen("TSLA");
+
+logX();
+
+function getOpen(symb) {
+	finnhubClient.quote(symb, (error, data, response) => { x = data.o; });
 }
-);
 
-
+function logX() {
+	sleep(500).then(() => {
+		console.log(x);
+	  })
+}
