1c0c27d55365cc99c6d165741d0c4279bde83817
0marA <theengineermachine@gmail.com>
2022-03-17 18:48:44 -0500

    Did some stuff for login entry

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
-import WelcomeMessage from './components/WelcomeMessage.vue'
+import Login from "./components/Login.vue"
 </script>
 
 <template>
-  <header>
-    <div class="welcome">
-      <WelcomeMessage msg="Welcome to Smiley Stocks!" />
+    <div class = "welcome">
+      <h1 style="position: relative; right: -70px">Welcome to Smiley Stocks!</h1>
+      <h2>Please enter your credentials or make a new account :)</h2>
     </div>
-  </header>
+
+    <div class = "login">
+      <Login/>
+    </div>
 </template>
+
+<style scoped>
+.welcome {
+  position: relative;
+  right: -50px;
+  top: 300px;
+  color: rgb(19, 83, 19);
+}
+
+.login {
+  position: relative;
+  right: -1200px;
+  top: 150px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+	<textarea class="textbox" type="textarea" id="USERNAME" rows="1" columns="3" placeholder="Username"> </textarea>
+	<textarea class="textbox" type="text" id="PASSWORD" rows="1" columns="3"> </textarea>
+    <br>
+    <button class="buttons" onclick="submit()" id="SUBMIT"> Submit </button>
+    <button class="buttons" onclick="newAccount()" id="newAccount"> New Account </button>
+</template>
+
+<style scoped>
+.textbox{
+    resize: none;
+    width: 10%;
+}
+
+.buttons {
+    position: relative;
+    right: -150px;
+}
+</style>
