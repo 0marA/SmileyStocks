239b0d7c996e0fd1ca0e1e223c89b20a59e5b0ef
0marA <theengineermachine@gmail.com>
2022-03-17 16:54:54 -0500

    Scrapping some bootstrap, rewriting login page

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6;
+  top: -10px;
+}
+</style>
