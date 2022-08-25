d6f0c4ce2fea606f8cb7de9708a6d36d5102534a
0marA <theengineermachine@gmail.com>
2022-08-24 22:14:38 -0500

    Switching to maps

--- backend/scripts/smileCalc.js
+++ backend/scripts/smileCalc.js
@@ -2,42 +2,43 @@ import { API_KEY } from "../../keys.js";
 import axios from "axios";
 
 let currentPrice;
-let sybmolsQueried = [];
-let sybmolsQueriedPrice = []; // Every time a price is queried, it'll be added here so that if there are symbols in non sequential order, there won't be a need to re-query the API
+let sybmolsQueried = new Map(); // Every time a price is queried, it'll be added here so that if there are symbols in non sequential order, there won't be a need to re-query the API
 
 export async function getCurrentPrice(symb) {
-    await axios
-        .get(`https://finnhub.io/api/v1/quote?symbol=${symb}&token=${API_KEY}`)
-        .then((response) => (currentPrice = response.data.c));
-    return currentPrice;
+    // await axios
+    //     .get(`https://finnhub.io/api/v1/quote?symbol=${symb}&token=${API_KEY}`)
+    //     .then((response) => (currentPrice = response.data.c));
+    // return currentPrice;
+    return 300;
 }
 
 export async function getSmiles() {
     let userData = await getUserStocks();
     let smiles = [];
-    //console.log(userData.userStocksMap.size);
-    for (const x of [...userData.userStocksMap.entries()]) console.log(x);
+    let userDataArray = Array.from([...userData.userStocksMap.entries()]); // This way we can peak at the next symbol in the array
+    userDataArray.sort();
 
-    //for (const x of userData.entries.keys()) {
-    // if (userData.get[x] != userData.stocks[x + 1]) {
-    //     // No need to make multiple API calls if the symbol is the same
+    for (let x = 0; x < userDataArray.length; x++) {
+        if (userDataArray[x] != userDataArray[x + 1]) {
+            // No need to make multiple API calls if the symbol is the same
+            if (sybmolsQueried.has(userDataArray[x])) {
+                // If the symbol has already been queried, then just get the price from the array
+                currentPrice = sybmolsQueried.get(userDataArray[x]);
+                console.log("Already found so not requerying " + currentPrice);
+            } else {
+                // If the symbol has not been queried, then query the API and add it to the map
+                currentPrice = await getCurrentPrice(userDataArray[x][0]);
+                sybmolsQueried.set(userDataArray[x], currentPrice);
+            }
+        }
 
-    //     if (sybmolsQueried.includes(userData.stocks[x])) {
-    //         // If the symbol has already been queried, then just get the price from the array
-    //         currentPrice = sybmolsQueriedPrice.indexOf(userData.stocks[x]);
-    //         console.log("Already found so not requerying " + currentPrice);
-    //     }
-    //     currentPrice = await getCurrentPrice(userData.stocks[x]);
-    //     sybmolsQueried.push(userData.stocks[x]);
-    //     sybmolsQueriedPrice.push(currentPrice);
-    //     console.log(currentPrice);
-    // }
-    // // let smile = currentPrice - stockBoughtPrices[x];
-    // // smiles.push(smile);
-    //}
-    //console.log(sybmolsQueried + " " + sybmolsQueriedPrice);
-
-    return "Smiles: " + userData.stocks;
+        // console.log(currentPrice);
+        // let smile = currentPrice - stockBoughtPrices[x];
+        // smiles.push(smile);
+    }
+    let symbolsQueriedArray = Array.from([...sybmolsQueried.entries()]);
+    console.log(symbolsQueriedArray);
+    return "Smiles: " + sybmolsQueried;
 }
 
 export async function seeMySymbols() {
@@ -46,8 +47,6 @@ export async function seeMySymbols() {
 }
 
 async function getUserStocks() {
-    // let stocks = [];
-    // let stockBoughtPrices = [];
     let userStocksMap = new Map();
 
     const res = await axios.get("/api/dashboard/getstocks"); // Returns a JSON of all the stuff in a users schema
@@ -65,8 +64,6 @@ async function getUserStocks() {
             Object.keys(stock), // Object.keys(stock) returns the keys of the object
             Object.values(stock)
         );
-        //stocks[x] = stocks[x].toString(); // Gets rid of the []s in the array
-        //stockBoughtPrices.push();
     }
     return { userStocksMap };
 }
