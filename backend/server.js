5643345b10e4d264625092f00ad39cb4898a52ad
0marA <theengineermachine@gmail.com>
2022-10-12 21:39:29 -0500

    express fix

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,18 @@ import express, { json, urlencoded } from "express";
 import connectDB from "./scripts/connection.js";
 import userRoutes from "./routes/userRoutes.js";
 import stockRoutes from "./routes/stockRoutes.js";
+import cors from "cors";
 
 const port = process.env.PORT || 4000;
 const app = express();
 
 connectDB();
 
-app.use(cors({}));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -16,8 +21,6 @@ app.use(urlencoded({ extended: false }));
 app.use("/api/", userRoutes);
 app.use("/api/dashboard", stockRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    express.use(express.static(__dirname, +"/frontend/dist"));
-}
+app.use(express.static("../frontend/dist"));
 
 app.listen(port, () => console.log(`Server started on ${port}!`));
