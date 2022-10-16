1858f62a0a9bc4828abaa5807e16c97f6c0e1ad2
0marA <theengineermachine@gmail.com>
2022-10-15 21:14:29 -0500

    Normal express

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ import express, { json, urlencoded } from "express";
 import connectDB from "./scripts/connection.js";
 import userRoutes from "./routes/userRoutes.js";
 import stockRoutes from "./routes/stockRoutes.js";
-import serveStatic from "serve-static";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 4000;
 
-connectDB();np
+connectDB();
+
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
@@ -18,6 +17,6 @@ app.use("/api/", userRoutes);
 app.use("/api/dashboard", stockRoutes);
 
 //app.use(express.static("../frontend/dist"));
-app.use(serveStatic(path.join(__dirname, "../frontend/dist")));
+//app.use(serveStatic(path.join(__dirname, "../frontend/dist")));
 
 app.listen(port, () => console.log(`Server started on ${port}!`));
