3ffe4debfe22073056238ed51537cbfa3df807d7
0marA <theengineermachine@gmail.com>
2022-10-21 19:14:25 -0500

    More testing

--- frontend/src/components/LoginBox.vue
+++ frontend/src/components/LoginBox.vue
@@ -5,42 +5,39 @@ export default {
     methods: {
         async login() {
             let username = document.getElementById("username").value;
-            await axios.post("/api/login/validate", {
-                data: {
-                    username: { username },
-                },
-            });
+            let doc = await axios.get("https://google.com", {});
+            console.log(doc);
         },
     },
 };
 </script>
 <template>
-        <input
-            class="textbox"
-            type="text"
-            id="username"
-            name="username"
-            placeholder="Username"
-            @click="login()"
-            style="position: absolute; top: -70px; left: 865px"
-        />
+    <input
+        class="textbox"
+        type="text"
+        id="username"
+        name="username"
+        placeholder="Username"
+        @click="login()"
+        style="position: absolute; top: -70px; left: 865px"
+    />
 
-        <div class="Buttons">
-            <button
-                id="REDIRECT_BUTTON"
-                class="buttons"
-                ref="btnToggle"
-                @click="$router.push('/newaccount')"
-            >
-                New Account
-            </button>
-            <input
-                class="buttons"
-                type="submit"
-                value="Submit"
-                style="position: absolute; left: 220px"
-            />
-        </div>
+    <div class="Buttons">
+        <button
+            id="REDIRECT_BUTTON"
+            class="buttons"
+            ref="btnToggle"
+            @click="$router.push('/newaccount')"
+        >
+            New Account
+        </button>
+        <input
+            class="buttons"
+            type="submit"
+            value="Submit"
+            style="position: absolute; left: 220px"
+        />
+    </div>
 </template>
 
 <style scoped>
