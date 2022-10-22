aeee6d85331600619e4d59301ee43cf70687cce1
0marA <theengineermachine@gmail.com>
2022-10-22 16:02:19 -0500

    bruh

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express, { json, urlencoded } from "express";
 import connectDB from "./scripts/connection.js";
 import userRoutes from "./routes/userRoutes.js";
 import stockRoutes from "./routes/stockRoutes.js";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 4000;
